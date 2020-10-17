fd811d738e4f23a18be6bbef952142c36c22f64f
KARTIK KHARIWAL <kartik.khariwal@gmail.com>
2020-10-18 02:46:52 +0530

    Add files via upload

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
@@ -7,35 +7,28 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
-# delimiter to tell its tsv 
-# quoting 3 to avoid quotes
 
 # Cleaning the texts
-import re    #pattern lib
-import nltk    # to import list of irrelevant word + imp for nlp
-nltk.download('stopwords')  #list of irrelevant word in many lang
-from nltk.corpus import stopwords    #inpoting list
-from nltk.stem.porter import PorterStemmer     #for stemming 
-
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 corpus = []
-for i in range(0, 1000): #cleaning one review at a time
-    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])   # to retian only a-zA-z char in review 
-    review = review.lower()   # tolower
-    review = review.split()   # string to list
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
     ps = PorterStemmer()
-    review = [ps.stem(word) for word in review if word not in set(stopwords.words('english'))]
-    #set used for faster implementation  
-    #ps.stem(word) for stemming  
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
     review = ' '.join(review)
-    #list of words to string
     corpus.append(review)
 
 # Creating the Bag of Words model
- #its a model  with review vs distinc words matrix where each word act as a feature 
 from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer(max_features = 1500)  #maxfeatures takes words acc to their freq
+cv = CountVectorizer(max_features = 1500)
 X = cv.fit_transform(corpus).toarray()
-y = dataset.iloc[:, -1].values
+y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -52,5 +45,3 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
-
-print("Efficiency = "+ str((cm[0][0]+cm[1][1])/(sum(sum(cm)))) )
